0fd12e8aedbd4b42217c5647adb367d256f22eaa
dave <djthorne@edu.uwaterloo.ca>
2018-09-11 16:33:03 -0400

    added --help option for cmd line args using yargs

--- app.js
+++ app.js
@@ -4,8 +4,36 @@ const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
 const notes = require('./notes.js');
-const argv = yargs.argv
+const titleOptions = {
+    describe: 'Title of the note',
+            demand: true,
+            alias: 't'
+};
+
+const bodyOptions = {
+    decribe: 'Body of the note',
+    demand: true,
+    alias: 'b'
+};
+
+const argv = yargs
+    .command('add', 'Add a new note', {
+        title: titleOptions,
+        body: bodyOptions
+    })
+    .command('list', 'List all notes')
+    .command('remove', 'Remove the specified note', {
+        title: titleOptions
+    })
+    .command('read', 'Read a specific note', {
+        title: titleOptions
+    })
+    .help()
+    .argv;
+
+
 let command = argv._[0];
+
 console.log('Command: ', command);
 console.log('Process ', process.argv);
 console.log('yargs ', argv);
@@ -22,12 +50,15 @@ if (command == 'add') {
     let allNotes = notes.getAll();
     console.log(`Printing ${allNotes.length} note(s)`);
     allNotes.forEach((note) => notes.logNote(note));
+
 } else if (command == 'read') {
     notes.readNote(argv.title);
+
 } else if (command == 'remove') {
     let noteRemoved = notes.removeNote(argv.title);
     let message = noteRemoved ? 'Note was removed' : 'Note not found';
     console.log(message);
+
 } else {
     console.log('Command not recognized');
 }
